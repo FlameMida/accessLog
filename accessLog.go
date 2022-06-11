6433ce8d6b37c9b4519539a0f3278e1b57378812
FlameMida <sweet.sakura@qq.com>
2022-06-11 16:57:27 +0800

    init

---
+++ accessLog.go
@@ -0,0 +1,256 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/errors"
+	"github.com/cloudwego/hertz/pkg/protocol"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/mattn/go-isatty"
+	"io"
+	"os"
+	"time"
+)
+
+type consoleColorModeValue int
+
+const (
+	autoColor consoleColorModeValue = iota
+	disableColor
+	forceColor
+)
+
+const (
+	green   = "\033[97;42m"
+	white   = "\033[90;47m"
+	yellow  = "\033[90;43m"
+	red     = "\033[97;41m"
+	blue    = "\033[97;44m"
+	magenta = "\033[97;45m"
+	cyan    = "\033[97;46m"
+	reset   = "\033[0m"
+)
+
+var consoleColorMode = autoColor
+
+// LoggerConfig defines the config for Logger middleware.
+type LoggerConfig struct {
+	// Optional. Default value is defaultLogFormatter
+	Formatter LogFormatter
+
+	// Output is a writer where logs are written.
+	// Optional. Default value is os.Stdout.
+	Output io.Writer
+
+	// SkipPaths is an url path array which logs are not written.
+	// Optional.
+	SkipPaths []string
+}
+
+// LogFormatter gives the signature of the formatter function passed to LoggerWithFormatter
+type LogFormatter func(params LogFormatterParams) string
+
+// LogFormatterParams is the structure any formatter will be handed when time to log comes
+type LogFormatterParams struct {
+	Request *protocol.Request
+
+	// TimeStamp shows the time after the server returns a response.
+	TimeStamp time.Time
+	// StatusCode is HTTP response code.
+	StatusCode int
+	// Latency is how much time the server cost to process a certain request.
+	Latency time.Duration
+	// ClientIP equals Context's ClientIP method.
+	ClientIP string
+	// Method is the HTTP method given to the request.
+	Method string
+	// Path is a path the client requests.
+	Path string
+	// Host is a Host the client requests.
+	Host string
+	// ErrorMessage is set if error has occurred in processing the request.
+	ErrorMessage string
+	// isTerm shows whether output descriptor refers to a terminal.
+	isTerm bool
+	// BodySize is the size of the Response Body
+	BodySize int
+	// Keys are the keys set on the request's context.
+	Keys map[string]any
+}
+
+// StatusCodeColor is the ANSI color for appropriately logging http status code to a terminal.
+func (p *LogFormatterParams) StatusCodeColor() string {
+	code := p.StatusCode
+
+	switch {
+	case code >= consts.StatusOK && code < consts.StatusMultipleChoices:
+		return green
+	case code >= consts.StatusMultipleChoices && code < consts.StatusBadRequest:
+		return white
+	case code >= consts.StatusBadRequest && code < consts.StatusInternalServerError:
+		return yellow
+	default:
+		return red
+	}
+}
+
+// MethodColor is the ANSI color for appropriately logging http method to a terminal.
+func (p *LogFormatterParams) MethodColor() string {
+	method := p.Method
+
+	switch method {
+	case consts.MethodGet:
+		return blue
+	case consts.MethodPost:
+		return cyan
+	case consts.MethodPut:
+		return yellow
+	case consts.MethodDelete:
+		return red
+	case consts.MethodPatch:
+		return green
+	case consts.MethodHead:
+		return magenta
+	case consts.MethodOptions:
+		return white
+	default:
+		return reset
+	}
+}
+
+// ResetColor resets all escape attributes.
+func (p *LogFormatterParams) ResetColor() string {
+	return reset
+}
+
+// IsOutputColor indicates whether can colors be outputted to the log.
+func (p *LogFormatterParams) IsOutputColor() bool {
+	return consoleColorMode == forceColor || (consoleColorMode == autoColor && p.isTerm)
+}
+
+// defaultLogFormatter is the default log format function Logger middleware uses.
+var defaultLogFormatter = func(param LogFormatterParams) string {
+	var statusColor, methodColor, resetColor string
+	if param.IsOutputColor() {
+		statusColor = param.StatusCodeColor()
+		methodColor = param.MethodColor()
+		resetColor = param.ResetColor()
+	}
+
+	if param.Latency > time.Minute {
+		param.Latency = param.Latency.Truncate(time.Second)
+	}
+	return fmt.Sprintf("[Hertz] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+		param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		statusColor, param.StatusCode, resetColor,
+		param.Latency,
+		param.ClientIP,
+		methodColor, param.Method, resetColor,
+		param.Path,
+		param.ErrorMessage,
+	)
+}
+
+// DisableConsoleColor disables color output in the console.
+func DisableConsoleColor() {
+	consoleColorMode = disableColor
+}
+
+// ForceConsoleColor force color output in the console.
+func ForceConsoleColor() {
+	consoleColorMode = forceColor
+}
+
+// Logger instances a Logger middleware that will write the logs to DefaultWriter.
+// By default,os.Stdout.
+func Logger() app.HandlerFunc {
+	return LoggerWithConfig(LoggerConfig{})
+}
+
+// LoggerWithFormatter instance a Logger middleware with the specified log format function.
+func LoggerWithFormatter(f LogFormatter) app.HandlerFunc {
+	return LoggerWithConfig(LoggerConfig{
+		Formatter: f,
+	})
+}
+
+// LoggerWithWriter instance a Logger middleware with the specified writer buffer.
+// Example: os.Stdout, a file opened in write mode, a socket...
+func LoggerWithWriter(out io.Writer, notLogged ...string) app.HandlerFunc {
+	return LoggerWithConfig(LoggerConfig{
+		Output:    out,
+		SkipPaths: notLogged,
+	})
+}
+
+// LoggerWithConfig instance a Logger middleware with config.
+func LoggerWithConfig(conf LoggerConfig) app.HandlerFunc {
+	formatter := conf.Formatter
+	if formatter == nil {
+		formatter = defaultLogFormatter
+	}
+
+	out := conf.Output
+	if out == nil {
+		out = os.Stdout
+	}
+
+	notLogged := conf.SkipPaths
+
+	isTerm := true
+
+	if w, ok := out.(*os.File); !ok || os.Getenv("TERM") == "dumb" ||
+		(!isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd())) {
+		isTerm = false
+	}
+
+	var skip map[string]struct{}
+
+	if length := len(notLogged); length > 0 {
+		skip = make(map[string]struct{}, length)
+
+		for _, path := range notLogged {
+			skip[path] = struct{}{}
+		}
+	}
+
+	return func(c context.Context, ctx *app.RequestContext) {
+		// Start timer
+		start := time.Now()
+		path := string(ctx.Request.URI().PathOriginal())
+		raw := string(ctx.Request.URI().QueryString())
+
+		// Process request
+		ctx.Next(c)
+
+		// Log only when path is not being skipped
+		if _, ok := skip[path]; !ok {
+			param := LogFormatterParams{
+				Request: ctx.Copy().GetRequest(),
+				isTerm:  isTerm,
+				Keys:    ctx.Keys,
+			}
+
+			// Stop timer
+			param.TimeStamp = time.Now()
+			param.Latency = param.TimeStamp.Sub(start)
+
+			param.ClientIP = ctx.ClientIP()
+			param.Method = string(ctx.Request.Header.Method())
+			param.StatusCode = ctx.Response.StatusCode()
+			param.Host = string(ctx.Request.Host())
+			param.ErrorMessage = ctx.Errors.ByType(errors.ErrorTypePrivate).String()
+
+			param.BodySize = len(ctx.Response.Body())
+
+			if raw != "" {
+				path = path + "?" + raw
+			}
+
+			param.Path = path
+
+			_, _ = fmt.Fprint(out, formatter(param))
+		}
+	}
+}
